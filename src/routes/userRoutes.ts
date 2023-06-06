d3ead1a1fdd0d10131a67ae1d438f9bba969a37f
unknown <rdsatsi001@st.ug.edu.gh>
2023-06-06 22:21:08 +0000

    initial commit

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,25 @@
+import { Prisma } from '@prisma/client'
+import {Router, NextFunction} from 'express'
+import {Request, Response} from 'express'
+import { prisma } from '../server'
+
+
+
+const userRoute = Router()
+
+userRoute.get('/',async(req:Request, res:Response)=>{
+
+    const users = await prisma.user.findMany({
+        where:{
+        },
+        include:{
+            Post:true
+        }
+    })
+
+    console.log(users)
+    res.status(200).json({users})
+})
+
+
+export default userRoute
---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import express, { Application, Response, Request } from 'express'
+import cors from 'cors'
+import { PrismaClient } from '@prisma/client'
+import userRoute from './routes/userRoutes'
+
+
+export const prisma = new PrismaClient()
+
+
+
+const app: Application = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(userRoute)
+
+
+
+async function main() {
+ app.listen(3500,()=>{
+    console.log("Ready")
+ })
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
